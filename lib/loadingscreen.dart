0a3a0ba8a990cc1bf807ca84a3c60d2ac96aa219
Sandu-2002 <sanjayapraveen2@gmail.com>
2026-03-17 21:42:36 +0530

    add loading screen

---
+++ lib/loadingscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class loadingscreenpage extends StatefulWidget {
+  const loadingscreenpage({super.key});
+
+  @override
+  State<loadingscreenpage> createState() => _loadingscreenpageState();
+}
+
+class _loadingscreenpageState extends State<loadingscreenpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
